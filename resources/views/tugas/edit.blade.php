e797e768078837f71a7d12f7dbb947e04becb778
Gitkamal123 <hafishkamal11@gmail.com>
2025-11-27 07:45:15 +0700

    perbaikan tombol lihat file

--- resources/views/tugas/edit.blade.php
+++ resources/views/tugas/edit.blade.php
@@ -361,11 +361,25 @@
                                             <path
                                                 d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z" />
                                         </svg>
-                                        <span>File saat ini: <a href="{{ Storage::url($tugas->file_path) }}"
-                                                target="_blank">Lihat File</a></span>
+                                        <span>File saat ini: <a href="{{ route('tugas.download', $t->id) }}" 
+                                            target="_blank" class="btn btn-sm btn-info text-white">Lihat File
+                                        </a>
                                     </div>
                                 @endif
 
+                                   {{-- @if($t->file_path)
+                                            <a href="{{ route('tugas.download', $t->id) }}" class="btn-download">
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                    viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                                                    <path
+                                                        d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                                                </svg>
+                                                Download
+                                            </a>
+                                        @else --}}
                                 <small class="help-text">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor"
                                         viewBox="0 0 16 16">
