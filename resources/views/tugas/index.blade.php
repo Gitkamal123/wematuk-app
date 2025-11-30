f108877c4e3ecf5ca6a7d7fc79750a528d4ae276
Gitkamal123 <hafishkamal11@gmail.com>
2025-11-30 18:07:44 +0700

    filter desain

--- resources/views/tugas/index.blade.php
+++ resources/views/tugas/index.blade.php
@@ -154,7 +154,7 @@
                     padding: 1.5rem;
                     box-shadow: 0 8px 32px rgba(0, 0, 0, 0.1);
                     border: 1px solid rgba(255, 255, 255, 0.2);
-                    margin-bottom: 4rem;
+                    margin-bottom: 10rem;
                 }
 
                 .form-control-custom,
