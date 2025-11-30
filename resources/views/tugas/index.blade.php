703309881eda94cf045bd47f276474fb0bcbd121
Gitkamal123 <hafishkamal11@gmail.com>
2025-11-30 20:28:32 +0700

    Auto update: 30/11/2025 20:28:32.31

--- resources/views/tugas/index.blade.php
+++ resources/views/tugas/index.blade.php
@@ -677,7 +677,7 @@
             .search-submit-btn {
                 position: absolute;
                 right: 5px;
-                top: 10%;
+                top: 15%;
                 transform: translateY(-15%);
                 height: calc(100% - 10px);
                 border-radius: 12px;
