fc3a4310ceb9eeb103609ed1c27bbf1e341b1278
Gitkamal123 <hafishkamal11@gmail.com>
2025-12-03 15:12:28 +0700

    Auto update: 03/12/2025 15:12:28.03

--- routes/web.php
+++ routes/web.php
@@ -388,7 +388,7 @@ Route::get('/add-unique-nrp-fixed', function () {
     // Gabungkan dengan middleware auth
 Route::middleware(['auth', 'prevent-back-history'])->group(function () {
 
-    // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/home', [TugasController::class, 'index'])->name('home');
     Route::get('/cari', [TugasController::class, 'cari'])->name('tugas.cari');
     Route::get('/laporan', [TugasController::class, 'cetakLaporan'])->name('laporan.cetak');
