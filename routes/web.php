b423e62fac772e6e4f6324a9df14e439def0c03a
Gitkamal123 <hafishkamal11@gmail.com>
2025-12-03 15:28:05 +0700

    Auto update: 03/12/2025 15:28:05.28

--- routes/web.php
+++ routes/web.php
@@ -400,7 +400,7 @@ Route::middleware(['auth'])->group(function () {
     // Route khusus untuk download file Base64
     Route::get('/tugas/{tugas}/download', [TugasController::class, 'downloadFile'])->name('tugas.download');
     // Route khusus untuk preview file
-Route::get('/tugas/{tugas}/preview', [TugasController::class, 'previewFile'])->name('tugas.preview');
+    Route::get('/tugas/{tugas}/preview', [TugasController::class, 'previewFile'])->name('tugas.preview');
 });
 
 // --- ROUTE ADMIN (WAJIB LOGIN + ADMIN) ---
